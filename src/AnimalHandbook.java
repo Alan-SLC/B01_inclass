b6e68ec86d61a2808ea1d0532eb1a1672cec84e4
Alan-SLC <sauron5169@gmail.com>
2024-07-03 14:07:30 +0800

    Finished

---
+++ src/AnimalHandbook.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class AnimalHandbook {
+    public static void main(String[] args) {
+        HashSet<String> animals = new HashSet<String>();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Animal Name? ");
+        String newAnimal = keyboard.nextLine();
+        animals.add(newAnimal);
+        while (!newAnimal.isEmpty()){
+            System.out.print("Animal Name? ");
+            newAnimal = keyboard.nextLine();
+            if(!newAnimal.isEmpty()) {
+                animals.add(newAnimal);
+            }
+        }
+        System.out.println("All registered animal names: " + animals);
+    }
+}
